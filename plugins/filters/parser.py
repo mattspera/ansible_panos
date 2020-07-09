95f097b2eacd1c8144d9d902ed0abe8739bd1845
Matthew Spera <speramatthew@gmail.com>
2020-07-09 12:02:19 +1000

    updated parser logic

--- plugins/filters/parser.py
+++ plugins/filters/parser.py
@@ -6,25 +6,25 @@ import ast
 
 from ansible.errors import AnsibleError, AnsibleFilterError
 
-def dev_dict_parser(a, *args, **kw):
+def dev_dict_parser(dg_or_temp_list):
     '''Custom parser which loops through API 'show dg/template' output,
     finds dg/template instances that contain a single device (in dict)
     and converts that dict to a list containing that dict. This allows
     Ansible to loop through dg/template inhabiting devices whether
     there be a single or multiple devices within dg/template'''
     # if string, convert to Python literal
-    if isinstance(a, str):
-        a = ast.literal_eval(a)
+    if isinstance(dg_or_temp_list, str):
+        dg_or_temp_list = ast.literal_eval(dg_or_temp_list)
 
-    if not isinstance(a, list):
+    if not isinstance(dg_or_temp_list, list):
         raise AnsibleFilterError('Object not a list.')
 
-    for item in a:
+    for item in dg_or_temp_list:
         if 'devices' in item:
             if isinstance(item['devices']['entry'], dict):
                 item['devices']['entry'] = [item['devices']['entry']]
 
-    return json.dumps(a)
+    return dg_or_temp_list
 
 class FilterModule(object):
     ''' PAN parser filters '''
